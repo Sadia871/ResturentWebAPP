0e60abc3dd1187b083ba84acacbec9d0efd2dfcc
Sadia <sadiasaleem871@gmail.com>
2023-03-12 01:28:17 +0500

    master

---
+++ 3rdpro/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Resturant from "./Components/basic/Resturant"
+
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ fontFamily:"initial",color:"purple",fontsize:"200px"}}>My 1st Project</h1>
+      <Resturant />
+    </div>
+  )
+}
+
+
+
