eeb9db20907f9e560534c2cf6d6e9e54b4d926bc
Sadia <sadiasaleem871@gmail.com>
2023-03-12 01:40:43 +0500

    Commit 2

--- 3rdpro/src/App.js
+++ 3rdpro/src/App.js
@@ -3,6 +3,8 @@ import Resturant from "./Components/basic/Resturant"
 
 
 export default function App() {
+  //TEST GITHUB
+  console,log("test")
   return (
     <div>
       <h1 style={{ fontFamily:"initial",color:"purple",fontsize:"200px"}}>My 1st Project</h1>
